4e882f77eab62c21d37bc010519b74e6c76f2424
rnakidi <laviravi@gmail.com>
2024-06-20 12:32:30 -0700

    Update ReverseBetween.java

--- LinkedList/ReverseBetween.java
+++ LinkedList/ReverseBetween.java
@@ -1,4 +1,25 @@
+92. Reverse Linked List II
+Description:
+Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
 
+Example 1:
+
+Input: head = [1,2,3,4,5], left = 2, right = 4
+Output: [1,4,3,2,5]
+    
+Example 2:
+
+Input: head = [5], left = 1, right = 1
+Output: [5]
+ 
+Constraints:
+
+The number of nodes in the list is n.
+1 <= n <= 500
+-500 <= Node.val <= 500
+1 <= left <= right <= n
+
+Follow up: Could you do it in one pass?
 
 /**
  * Definition for singly-linked list.
@@ -12,28 +33,25 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        int i = 0;
-        ListNode temp = head;
-        ListNode leftNode = null;
-        ListNode rightNode = null;\
+        if (head == null || left == right) {
+            return head;
+        }      
 
-        while (null != temp) {
-            i++;
-            if (i == left) {
-                leftNode = temp;
-            }
-            
-            if (i == right) {
-                rightNode = temp;
-            }
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy;
 
-            temp = temp.next;
+        for (int i = 0; i < left - 1; ++i) {
+            prev = prev.next;
         }
-        
-        int val = leftNode.val;
-        leftNode.val = rightNode.val;
-        rightNode.val = val;
+ 
+        ListNode tail = prev.next;
+        for (int i = 0; i < right - left; ++i) {
+            ListNode cache = tail.next;
+            tail.next = cache.next;
+            cache.next = prev.next;
+            prev.next = cache;
+        }
 
-        return head;
+        return dummy.next;
     }
 }
