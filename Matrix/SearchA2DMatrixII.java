885e890f98e1222dff0d75f6000a0d00eceee0ad
rnakidi <laviravi@gmail.com>
2024-06-22 18:17:10 -0700

    SearchA2DMatrixII

---
+++ Matrix/SearchA2DMatrixII.java
@@ -0,0 +1,84 @@
+240. Search a 2D Matrix II
+Description
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+
+Example 1:
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true
+  
+Example 2:
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+Output: false 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= n, m <= 300
+-109 <= matrix[i][j] <= 109
+All the integers in each row are sorted in ascending order.
+All the integers in each column are sorted in ascending order.
+-109 <= target <= 109
+
+Solution (T:O(nlogm), S:O(1)) 
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            if (search(matrix[i], target)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean search(int[] matrix, int target) {
+        int low = 0;
+        int high = matrix.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+            if (matrix[mid] == target) {
+                return true;
+            }
+
+            if (matrix[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+}
+
+Solution (T:O(m+n), S:O(n))
+  
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int r = 0;
+        int c = cols - 1;
+        while (r < rows && c >= 0) {
+            if (matrix[r][c] == target) {
+                return true;
+            }
+            
+            if (matrix[r][c] < target) {
+                r++;
+            } else {
+                c--;
+            }
+        }
+        return false;
+    }
+}
