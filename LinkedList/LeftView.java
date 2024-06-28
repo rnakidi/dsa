7ddd18008858578a756112d743528b932fb536be
rnakidi <laviravi@gmail.com>
2024-06-28 10:52:51 -0700

    Create LeftView.java

---
+++ LinkedList/LeftView.java
@@ -0,0 +1,40 @@
+Binary Tree Left Side View
+
+  /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    static int maxLevel;
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        maxLevel = 0;
+        util(root, ans, 1);
+        return ans;        
+    }
+
+    public void util(TreeNode root, List<Integer> ans, int level) {
+        if (null == root) {
+            return;
+        }
+
+        if (maxLevel < level) {
+            ans.add(root.val);
+            maxLevel = level;
+        }
+
+        util(root.right, ans, level + 1);
+        util(root.left, ans, level + 1);        
+    }
+}
