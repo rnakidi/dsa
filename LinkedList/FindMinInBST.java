67f19443ddb933c887834f1ab2468223a2bc3f04
rnakidi <laviravi@gmail.com>
2024-06-28 14:40:51 -0700

    Create FindMinInBST.java

---
+++ LinkedList/FindMinInBST.java
@@ -0,0 +1,26 @@
+Find Min In BST
+
+Solution(T:O(n), S:O(n))
+
+class Solution {
+  
+	public int findMin(TreeNode root) {
+		
+		if (null == root) {
+			  return Integer.MAX_VALUE;
+		}
+		
+		int min = root.val;
+		int leftMin = findMin(root.left);
+		int rightMin = findMin(root.right);
+		
+		if (leftMin < min) {
+			min = leftMin;
+		}
+		
+		if (rightMin < min) {
+			min = rightMin;
+		}		
+		return min;		
+	}
+}
