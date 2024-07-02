1b0e30768528429d9ba6658b58d7f13ca8b297e1
rnakidi <laviravi@gmail.com>
2024-07-01 18:38:33 -0700

    Create FindAllAnagramsInAString.java

---
+++ Strings/FindAllAnagramsInAString.java
@@ -0,0 +1,73 @@
+438. Find All Anagrams in a String
+Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+  
+Input: s = "cbaebabacd", p = "abc"
+Output: [0,6]
+Explanation:
+The substring with start index = 0 is "cba", which is an anagram of "abc".
+The substring with start index = 6 is "bac", which is an anagram of "abc".
+  
+Example 2:
+  
+Input: s = "abab", p = "ab"
+Output: [0,1,2]
+Explanation:
+The substring with start index = 0 is "ab", which is an anagram of "ab".
+The substring with start index = 1 is "ba", which is an anagram of "ab".
+The substring with start index = 2 is "ab", which is an anagram of "ab". 
+
+Constraints:
+
+1 <= s.length, p.length <= 3 * 104
+s and p consist of lowercase English letters.
+
+Solution(T:O(n+m), S:O(n))
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+
+        if (p.length() > s.length()) {
+            return result;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : p.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        int len = Integer.MAX_VALUE;
+        int count = map.size();
+        int begin = 0;
+        int end = 0;
+        while (end < s.length()) {
+            char ch = s.charAt(end);
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) - 1);
+                if (map.get(ch) == 0) {
+                    count--;
+                } 
+            }
+
+            end++;
+
+            while (count == 0) {
+                ch = s.charAt(begin);
+                if (map.containsKey(ch)) {
+                    map.put(ch, map.get(ch) + 1);
+                    if (map.get(ch) > 0) {
+                        count++;
+                    }
+                }
+                if (end - begin == p.length()) {
+                    result.add(begin);
+                 }
+                begin++;
+            }
+        }
+        return result;
+    }
+}
