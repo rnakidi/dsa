845efcb932274edbb0fdcfa92eb0425ef8760126
rnakidi <laviravi@gmail.com>
2024-07-06 11:11:30 -0700

    Create NthDigit.java

---
+++ NthDigit.java
@@ -0,0 +1,40 @@
+400. Nth Digit
+Desciption
+Given an integer n, return the nth digit of the infinite integer sequence [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...].
+
+Example 1:
+
+Input: n = 3
+Output: 3
+
+Example 2:
+
+Input: n = 11
+Output: 0
+Explanation: The 11th digit of the sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ... is a 0, which is part of the number 10.
+
+Constraints:
+
+1 <= n <= 231 - 1
+Solution
+1. Find the length of the number where the nth digit is from.
+2. Find the actual number where the nth digit is from.
+3. Find the nth digit and return.
+class Solution {
+	public int findNthDigit(int n) {
+		int len = 1;
+		long count = 9;
+		int start = 1;
+
+		while (n > len * count) {
+			n -= len * count;
+			len += 1;
+			count *= 10;
+			start *= 10;
+		}
+
+		start += (n - 1) / len;
+		String s = Integer.toString(start);
+		return Character.getNumericValue(s.charAt((n - 1) % len));
+	}
+}
