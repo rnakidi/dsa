554db9c2527fece76b6dd3d5c6c53b7af6d5a2e0
rnakidi <laviravi@gmail.com>
2024-07-25 19:24:41 -0700

    Update CountPrimes.java

--- Arrays/CountPrimes.java
+++ Arrays/CountPrimes.java
@@ -19,7 +19,7 @@ Constraints:
 
 0 <= n <= 5 * 106
 
-Solution
+Solution(T:O(n*logn), S:O(1))
 class Solution {
     public int countPrimes(int n) {
         int count = 0;
@@ -38,5 +38,36 @@ class Solution {
         }
 
         return count;
+    }
+}
+
+Solution(T:O(sqrt(n)), S:O(1))
+class Solution {
+    public int countPrimes(int n) {
+        if (0 == n || 1 == n) {
+            return 0;
+        }
+
+        int[] primes = new int[n];
+        primes[0] = 1;
+        primes[1] = 1;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) { // Math.sqrt can be repleced by Math.pow(n, 0.5) or i * i
+            if (primes[i] == 0) {
+                int j = i * 2; // mark multiples of i
+                while (j < n) {
+                    primes[j] = 1;
+                    j += i;
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (primes[i] == 0) {
+                ans++;            }
+        }
+
+        return ans;
     }
 }
