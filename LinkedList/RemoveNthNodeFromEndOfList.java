3dc278b8f9937c7b396fefaa737d7d18397216a5
rnakidi <laviravi@gmail.com>
2024-11-05 14:51:20 -0800

    Update and rename RemoveNthFromEnd.java to RemoveNthNodeFromEndOfList.java

--- LinkedList/RemoveNthNodeFromEndOfList.java
+++ LinkedList/RemoveNthNodeFromEndOfList.java
@@ -1,4 +1,4 @@
-19. Remove Nth Node From End of List
+19. Remove Nth Node From End of List - https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
 Description
 Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
