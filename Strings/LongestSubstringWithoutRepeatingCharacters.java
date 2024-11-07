58c7226ccfdbcab9a7b7e3675a02a6f4b4c744ef
rnakidi <laviravi@gmail.com>
2024-11-07 11:46:56 -0800

    Update LongestSubstringWithoutRepeatingCharacters.java

--- Strings/LongestSubstringWithoutRepeatingCharacters.java
+++ Strings/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,7 +1,5 @@
-3. Longest Substring Without Repeating Characters
-Given a string s, find the length of the longest 
-substring
- without repeating characters.
+3. Longest Substring Without Repeating Characters - https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ Given a string s, find the length of the longest substring without repeating characters.
 
 Example 1:
 
