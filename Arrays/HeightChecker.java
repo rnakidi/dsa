1923b5f2374c9b13636f6d30b5ff31ee63939d07
rnakidi <laviravi@gmail.com>
2024-11-15 13:25:22 -0800

    Create HeightChecker.java

---
+++ Arrays/HeightChecker.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] temp = new int[heights.length];
+        for (int i = 0; i < heights.length; i++) {
+            temp[i] = heights[i];
+        }
+        
+        int count = 0;
+        Arrays.sort(temp);
+        for (int i = 0; i < temp.length; i++) {
+            if (temp[i] != heights[i]) {
+                count++;
+            }
+        } 
+        return count;
+    }
+}
