84a976003e565991698cc2b0d48ca5736545f8c8
rnakidi <laviravi@gmail.com>
2024-11-19 15:52:57 -0800

    Create SquaresOfASortedArray.java

---
+++ Arrays/SquaresOfASortedArray.java
@@ -0,0 +1,24 @@
+977. Squares of a Sorted Array - https://leetcode.com/problems/squares-of-a-sorted-array/description/ - EASY
+  
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+         for (int i = 0; i < nums.length; i++) {
+            nums[i] = (nums[i] * nums[i]);
+        }
+
+        int head = 0;
+        int tail = nums.length - 1;
+        int[] result = new int[nums.length];
+        for (int pos = nums.length - 1; pos >=0; pos--) {
+            if (nums[head] > nums[tail]) {
+                result[pos] = nums[head];
+                head++;
+            } else {
+                result[pos] = nums[tail];
+                tail--;
+            }
+        }
+
+        return result;
+    }
+}
