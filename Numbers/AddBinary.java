34df0639a0842c5f996e612281add9fbd0b04073
rnakidi <laviravi@gmail.com>
2024-11-21 15:41:17 -0800

    Update AddBinary.java

--- Numbers/AddBinary.java
+++ Numbers/AddBinary.java
@@ -2,6 +2,24 @@
 
 class Solution {
     public String addBinary(String a, String b) {
+        int lenA = a.length() - 1;
+        int lenB = b.length() - 1;
+        int carry = 0;
+        int sum = 0;
+        String output = "";
+        while (lenA >= 0 || lenB >= 0 || carry != 0) { // carry == 1
+            sum = carry;
+            sum += lenA >= 0 ? (a.charAt(lenA--) - '0') : 0;
+            sum += lenB >= 0 ? (b.charAt(lenB--) - '0') : 0;
+            output = (sum%2) + output;
+            carry = sum/2;
+        }
+        return output;
+    }
+}
+
+class Solution {
+    public String addBinary(String a, String b) {
         int aNum = Integer.parseInt(a, 2);
         int bNum = Integer.parseInt(b, 2);
         return Integer.toBinaryString(aNum + bNum);
