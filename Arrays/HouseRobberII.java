1da16f5aecf87f6e2697d5d370a3e3704d2855d5
rnakidi <laviravi@gmail.com>
2024-11-24 16:38:32 -0800

    Update HouseRobberII.java

--- Arrays/HouseRobberII.java
+++ Arrays/HouseRobberII.java
@@ -22,3 +22,34 @@ class Solution {
         return max;
     }
 }
+
+Approach - II(Same as HouseRobberyI:
+
+class Solution {
+    public int rob(int[] nums) {
+        int len = nums.length;
+        if (1 == len) {
+            return nums[0];
+        }
+
+        return Math.max(max(nums, 0, len-2), max(nums, 1, len-1));
+    }
+
+    private int max(int[] nums, int low, int end) {
+        int[] temp = new int[end - low + 1];
+        int j = 0;
+        for (int i = low; i <= end; i++) {
+            temp[j++] = nums[i];
+        }
+
+        int n = temp.length;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = temp[0];
+        for (int i = 1; i < n; i++) {
+            dp[i+1] = Math.max(dp[i], dp[i-1] + temp[i]);           
+        }
+
+        return dp[n];
+    }
+}
