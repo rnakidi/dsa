4ecbc0d25ada43620d77816ee3be12c9b4b139bb
rnakidi <laviravi@gmail.com>
2025-02-14 09:03:14 -0800

    Update WordBreak.java

--- Strings/WordBreak.java
+++ Strings/WordBreak.java
@@ -56,3 +56,28 @@ class Solution {
         return false;
     }
 }  
+
+Complexity
+Time complexity: O(n∗m∗k)
+n is length of input string.
+m is number of words in wordDict
+k is average size of substrings.
+
+Space complexity: O(n)
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (String w : wordDict) {
+                int start = i - w.length();
+                if (start >= 0 && dp[start] && s.substring(start, i).equals(w)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];        
+    }
+}
