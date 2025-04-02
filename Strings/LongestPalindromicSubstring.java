d8586a5458a28dd5f3ace5c0844bdc87d7959e38
rnakidi <laviravi@gmail.com>
2025-04-02 09:12:40 -0700

    Update LongestPalindromicSubstring.java

--- Strings/LongestPalindromicSubstring.java
+++ Strings/LongestPalindromicSubstring.java
@@ -2,45 +2,43 @@
   
 class Solution {
     public String longestPalindrome(String s) {
-    if (null == s) {
-			  return "";
-		}
-		
-		if (s.length() < 2) {
-			return s;
-		}
-		
-		helper(s, 0);
-		
-		return len == Integer.MAX_VALUE ? "" : s.substring(head, head + len);
-	}
+	    if (null == s) {
+		    return "";
+	    }
+	    
+	    if (s.length() < 2) {
+		    return s;
+	    }
+	    
+	    helper(s, 0);
+	    return len == Integer.MAX_VALUE ? "" : s.substring(head, head + len);
+    }
+	
+    private void helper(String s, int l) {
+	    if (l == s.length()) {
+		    return;
+	    }
+	    
+	    for (int i = l; i < s.length(); i++) {
+		    if (isPalindrome(s, l, i)) {
+			    if (i - l + 1 > len) { // end - begin + 1
+				    len = i - l + 1;
+				    head = l;
+			    }
+			    helper(s, i+1);
+		    }
+	    }
+    }
 
-	private void helper(String s, int l) {
-		if (l == s.length()) {
-			return;
-		}
-		
-		for (int i = l; i < s.length(); i++) {
-			if (isPalindrome(s, l, i)) {
-				if (i - l + 1 > len) {
-					len = i - l + 1;
-					head = l;
-				}
-				helper(s, i+1);
-			}
-		}
-	}
-
-	private boolean isPalindrome(String s, int l, int r) {
-		while (l <= r) {
-			if (s.charAt(l) != s.charAt(r)) {
-				return false;
-			}
-			l++;
-			r--;
-		}
-
-		return true;
-	}
-}
+    private boolean isPalindrome(String s, int l, int r) {
+	    while (l <= r) {
+		    if (s.charAt(l) != s.charAt(r)) {
+			    return false;
+		    }
+		    l++;
+		    r--;
+	    }
+	    return true;
+    }
+ }
 }
