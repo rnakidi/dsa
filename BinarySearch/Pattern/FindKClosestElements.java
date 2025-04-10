9e19efbf02c4ea02af2489efe28bb664d0e78ed2
rnakidi <laviravi@gmail.com>
2025-04-10 11:49:19 -0700

    Update and rename FindKClosestElements.java to BinarySearch/Pattern/FindKClosestElements.java

--- BinarySearch/Pattern/FindKClosestElements.java
+++ BinarySearch/Pattern/FindKClosestElements.java
@@ -1,6 +1,7 @@
 658. Find K Closest Elements
 Description
-Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. 
+The result should also be sorted in ascending order.
 
 An integer a is closer to x than an integer b if:
 
