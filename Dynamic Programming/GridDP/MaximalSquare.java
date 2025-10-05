d333a7b79bb6c51b29dbc3b21f5f1847aac73112
rnakidi <laviravi@gmail.com>
2025-10-05 15:17:20 -0700

    Update MaximalSquare.java

--- Dynamic Programming/GridDP/MaximalSquare.java
+++ Dynamic Programming/GridDP/MaximalSquare.java
@@ -1,13 +1,4 @@
 221. Maximal Square - https://leetcode.com/problems/maximal-square/ - Medimum
-Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
-
-Example 1:
-Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
-Output: 4
-
-Example 2:
-Input: matrix = [["0","1"],["1","0"]]
-Output: 1
 
 class Solution {
     public int maximalSquare(char[][] matrix) {
